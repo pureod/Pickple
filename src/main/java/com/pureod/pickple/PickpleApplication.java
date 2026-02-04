bd60691144f41d1d01861ffe21fcc11789b06ca5
pureod <sjo06102@gmail.com>
2026-02-04 21:51:04 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/pureod/pickple/PickpleApplication.java
@@ -0,0 +1,13 @@
+package com.pureod.pickple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pureod/pickple/PickpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pureod.pickple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
