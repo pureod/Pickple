3a09a5fcc8507c10431bcb724247686e7133866e
pureod <sjo06102@gmail.com>
2026-02-09 20:43:16 +0900

    feat: 사용자 엔티티(User) 생성

---
+++ src/main/java/com/pureod/pickple/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.pureod.pickple.domain.user.entity;
+
+import com.pureod.pickple.domain.user.enums.Role;
+import com.pureod.pickple.global.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseUpdatableEntity {
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "password", length = 20, nullable = false)
+    private String password;
+
+    @Column(name = "profile_image_url", columnDefinition = "TEXT")
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "locked", nullable = false)
+    private boolean locked;
+
+}
